fd0f924bc4650622d9d13a655acaecbb1aaa24fc
Julian Jozic <jjozic98@gmail.com>
2020-06-26 23:48:35 +0200

    Added material design cards with stats
    - added cards with material design
    - cards get updated data for infected, recovered and deaths from the api
    - displayed the number with react-countup
    - working on a chart with chart.js

--- src/api/index.js
+++ src/api/index.js
@@ -3,10 +3,29 @@ import axios from 'axios'
 const url = 'https://covid.mathdro.id/api'
 
 export const fetchData = async () => {
-    try{
-        const response = await axios.get(url)
+    try {
+        //we filter the data from the response
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url)
 
-        return response
+        //returns only the JSON content which we  and need
+        return { confirmed, recovered, deaths, lastUpdate }
+    } catch (error) {
+
+    }
+}
+
+export const fetchDailyData = async () => {
+    try {
+        //we filter the data from the response
+        const { data } = await axios.get(`${url}/daily`)
+
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate
+        }))
+        //returns only the JSON content which we  and need
+        return modifiedData
     } catch (error) {
 
     }
