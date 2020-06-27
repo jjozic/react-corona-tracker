d066de4b2e48208e3ff4aaf740dab90fc7dc2c7d
Julian Jozic <jjozic98@gmail.com>
2020-06-27 13:07:06 +0200

    Updates

---
+++ precache-manifest.269c833246d7f1d2a65e684c827fa53d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad01011450a53d4d1c428710c970116c",
+    "url": "/react-corona-tracker/index.html"
+  },
+  {
+    "revision": "167eb39864466c6afeb4",
+    "url": "/react-corona-tracker/static/css/main.f40086f7.chunk.css"
+  },
+  {
+    "revision": "ead72bd374c2f1bef8cd",
+    "url": "/react-corona-tracker/static/js/2.dd1dc1af.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-corona-tracker/static/js/2.dd1dc1af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "167eb39864466c6afeb4",
+    "url": "/react-corona-tracker/static/js/main.cf45afb2.chunk.js"
+  },
+  {
+    "revision": "cd06ae12231c84d4f78f",
+    "url": "/react-corona-tracker/static/js/runtime-main.6115be80.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-corona-tracker/static/media/image.d7265326.png"
+  }
+]);
