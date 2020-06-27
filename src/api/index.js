51e72a3ecf1552ede972cc355f00543e1116faad
Julian Jozic <jjozic98@gmail.com>
2020-06-27 11:24:49 +0200

    Finished funcionality

--- src/api/index.js
+++ src/api/index.js
@@ -2,15 +2,21 @@ import axios from 'axios'
 
 const url = 'https://covid.mathdro.id/api'
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+    let changeableUrl = url
+
+    if(country){
+        changeableUrl = `${url}/countries/${country}`
+    }
+
     try {
         //we filter the data from the response
-        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url)
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl)
 
         //returns only the JSON content which we  and need
         return { confirmed, recovered, deaths, lastUpdate }
     } catch (error) {
-
+        console.log(error)
     }
 }
 
@@ -27,6 +33,17 @@ export const fetchDailyData = async () => {
         //returns only the JSON content which we  and need
         return modifiedData
     } catch (error) {
+        console.log(error)
+    }
+}
 
+export const fetchCountries = async () => {
+    try {
+        //we filter the data from the response
+        const { data: { countries }} = await axios.get(`${url}/countries`)
+
+        return countries.map((country) => country.name)
+    } catch (error) {
+        console.log(error)
     }
 }
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react'
 import { fetchDailyData } from '../../api'
 import { Line, Bar } from 'react-chartjs-2'
 
-const Chart = () => {
+import styles from './Chart.module.css'
+
+const Chart = ({ data: { confirmed, recovered, deaths }, country}) => {
     const [dailyData, setDailyData] = useState([])
 
     useEffect(() => {
@@ -11,7 +13,7 @@ const Chart = () => {
         }
 
         fetchAPI()
-    }, [dailyData])
+    }, [])
 
     const lineChart = (
         dailyData.length
@@ -36,7 +38,34 @@ const Chart = () => {
         ) : null
     )
 
-    return (<h1>Chart</h1>)
+    const barChart = (
+        confirmed
+        ? (<Bar 
+            data={{
+                labels: ['Infected', 'Recovered', 'Deaths'],
+                datasets: [{
+                    label: 'People',
+                    backgroundColor: [
+                        'rgba(0, 0, 255, 0.5)',
+                        'rgba(0, 255, 0, 0.5)',
+                        'rgba(255, 0, 0, 0.5)',
+                    ],
+                    data: [confirmed.value, recovered.value, deaths.value]
+                }]
+            }}
+            options={{
+                legend: { display: false },
+                title: { display: true, text:`Current state in ${country}`},
+            }}
+        />
+        ) : null
+    )
+
+    return (
+        <div className={styles.container}>
+            {country ? barChart : lineChart}
+        </div>
+    )
 }
 
 export default Chart
